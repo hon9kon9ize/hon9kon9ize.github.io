a2f0861d0d991c2861008cbc88c7f6d9b2496991
Devon Crebbin <devon@artvuu.group>
2024-10-20 17:31:21 +1100

    added error handling for webgl failure edge case

--- src/components/underlay.tsx
+++ src/components/underlay.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Stats } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import * as THREE from "three"
@@ -14,6 +14,22 @@ export const Underlay: React.FC<{
   mode?: "ring" | "model"
 }> = ({ mode = "model" }) => {
   const pointRef = useRef<THREE.Mesh>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const gl = useRef<WebGLRenderingContext>()
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    try{
+      gl.current = new THREE.WebGLRenderer({ canvas: canvasRef.current!,
+        antialias: false,
+        stencil: false,
+        depth: false,
+        alpha: true,
+      }).getContext();
+    } catch (e) {
+      setError(e as Error)
+    }
+  }, [canvasRef])
 
   return (
     <div
@@ -26,9 +42,11 @@ export const Underlay: React.FC<{
         zIndex: 0,
       }}
     >
+      {!error && (
       <Canvas
+        ref={canvasRef}
         shadows={false}
-        gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
+        gl={gl.current}
         camera={{ position: [0, 0, 18], fov: 2, near: 1, far: 100 }}
       >
         <PCDModel
@@ -42,7 +60,8 @@ export const Underlay: React.FC<{
           // rotation={new THREE.Euler(-0.2, 0, 0)}
         />
         {isDev && <Stats />}
-      </Canvas>
+        </Canvas>
+      )}
     </div>
   )
 }
